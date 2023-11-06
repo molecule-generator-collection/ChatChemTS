2776f1732d527fbae801eaef6d30d1e3c9c73057
sishida21 <29348731+sishida21@users.noreply.github.com>
2023-11-06 18:44:30 +0900

    feat: add file management tools

--- tools.py
+++ tools.py
@@ -1,22 +1,34 @@
+import os
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.prompt import PROMPT
-from langchain.agents.agent_toolkits import create_python_agent
-from langchain.tools.python.tool import PythonREPLTool
+from langchain_experimental.agents.agent_toolkits import create_python_agent
+from langchain_experimental.tools.python.tool import PythonREPLTool
 from langchain.tools import Tool
+from langchain.agents.agent_toolkits import FileManagementToolkit
 from langchain.agents.agent_types import AgentType
 from langchain.agents import initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from langchain.schema import StrOutputParser
 from pydantic.v1 import BaseModel, Field
 
 from prompts import PREFIX_REWARD, PREFIX_CONFIG
 
 
 def prepare_tools(verbose=True):
+    root_dir = "./chatbot_data"
+    os.makedirs(root_dir, exist_ok=True)
+    file_tools = FileManagementToolkit(root_dir=root_dir, 
+                                       selected_tools=[
+                                           'read_file',
+                                           'write_file',
+                                           'list_directory',
+                                           'move_file']).get_tools()
+
     return [
         create_reward_generator_tool(),
         create_config_generator_tool(),
-    ]
+    ] + file_tools
 
 
 class RewardGeneratorInput(BaseModel):
@@ -50,11 +62,11 @@ class ConfigGeneratorInput(BaseModel):
 
 def create_config_generator_tool(verbose=True):
     llm_chain = ConversationChain(
-        llm=ChatOpenAI(temperature=0),
+        llm=ChatOpenAI(temperature=0, streaming=True),
         verbose=verbose,
         prompt=PromptTemplate.from_template(PREFIX_CONFIG)+PROMPT,
     )
-    description = """Usefule for when you need to generate config files of molecule generators, such as ChemTSv2, related to the query. 
+    description = """Usefule for when you need to write config files of molecule generators, such as ChemTSv2, related to the query. 
         The input to this tool should be a `str` format, not `dict`.
         The input should be a phrase, not a single word."""
     config_tool = Tool.from_function(
